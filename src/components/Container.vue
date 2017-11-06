ae6446f7a5732f519b78a3108b290438ae2f032f
Jon Wong-Lam <jonwonglam@gmail.com>
2017-11-05 17:07:15 -0800

    Refactor sidebar and list of bookmarks into vue components

---
+++ src/components/Container.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app-container" class="flex ">
+    <aside class="sidebar">
+      <folder-list></folder-list>
+    </aside>
+    <main class="main-container">
+      <header class="header">
+
+      </header>
+      <div class="content-container">
+        <bookmarks-list></bookmarks-list>
+      </div>
+    </main class="main-container">
+  </div>
+</template>
+
+<script>
+import BookmarksList from './BookmarksList'
+import FolderList from './folders/FolderList'
+
+export default {
+  components: {
+    BookmarksList,
+    FolderList
+  }
+}
+</script>
+
+<style>
+  #app-container {
+    width: 100%;
+    height: 100%;
+  }
+  .flex {
+    display: flex;
+  }
+  .sidebar {
+    border-right: 1px solid #eeeeee;
+    height: 100%;
+    position: fixed;
+    width: 350px;
+  }
+  .main-container {
+    width: 100%;
+    height: 100%;
+    margin-left: 350px;
+  }
+  .content-container {
+    margin-top: 30px;
+  }
+  .header {
+    border-bottom: 1px solid #eeeeee;
+    width: 100%;
+    height: 66px;
+  }
+
+</style>
